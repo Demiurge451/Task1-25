484cc69197ed049edafb3ef40514cc0e001d3e5e
Konstantin Lugovskoy <tornadov.costya@yandex.ru>
2022-05-27 12:04:19 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MainForm2 m = new MainForm2(3, 2);
+        //при изменении countSection меняется количество секций в светофоре, при изменение timeChangeСolor меняется
+        //время через, которое будут загораться секции
+    }
+}
